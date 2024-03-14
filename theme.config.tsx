90f12d87d1bb44f29365b32ef2d29b86b68d3827
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-03-13 20:01:16 -0700

    Add image example

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,15 +5,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs';
 const config: DocsThemeConfig = {
   logo: <span>Medical Multi Modal Model</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/UC-Berkeley-I-School/mids-210-medical-multi-modal-model-spring24',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/UC-Berkeley-I-School/mids-210-medical-multi-modal-model-spring24',
   footer: {
     text: 'Medical Multi Modal Models Capstone Project - MIDS 210 Spring 2024',
   },
   useNextSeoProps: () => {
     const { asPath } = useRouter();
-    if (asPath !== '/') {
+    if (asPath !== '/mids-210-medical-multi-modal-model-spring24') {
       return {
         titleTemplate: '%s – Medical Multi Modal Model',
       };
