d1b9c7af40f901f3573cf2191b652866a863441d
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-03-13 20:16:42 -0700

    Fix SEO

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,15 +13,41 @@ const config: DocsThemeConfig = {
   },
   useNextSeoProps: () => {
     const { asPath } = useRouter();
-    if (asPath !== '/mids-210-medical-multi-modal-model-spring24') {
+    if (asPath !== '/') {
       return {
         titleTemplate: '%s – Medical Multi Modal Model',
       };
     }
+    return {
+      titleTemplate: '%s',
+    };
   },
   feedback: {
     content: '',
   },
+  head: (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/mids-210-medical-multi-modal-model-spring24/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/mids-210-medical-multi-modal-model-spring24/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/mids-210-medical-multi-modal-model-spring24/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/mids-210-medical-multi-modal-model-spring24/site.webmanifest" />
+      <link rel="mask-icon" href="/mids-210-medical-multi-modal-model-spring24/safari-pinned-tab.svg" color="#5bbad5" />
+    </>
+  ),
 };
 
 export default config;
