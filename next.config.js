b0169639ccb1e7f22817c10f36e534d1f86e6b59
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-03-16 17:04:04 -0700

    onnx test template

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
@@ -7,12 +10,37 @@ const withNextra = require('nextra')({
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-  ...withNextra(),
+module.exports = withNextra({
   rewrites: undefined,
   output: 'export',
   basePath: '/mids-210-medical-multi-modal-model-spring24',
   images: {
     unoptimized: true,
   },
-};
+  webpack: (config, {}) => {
+    // config.resolve.extensions.push('.ts', '.tsx', '.mdx');
+    config.resolve.fallback = { fs: false };
+
+    config.plugins.push(
+      new NodePolyfillPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
+            to: 'static/chunks/pages',
+          },
+          {
+            from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+            to: 'static/chunks/pages',
+          },
+          {
+            from: './model',
+            to: 'static/chunks/pages',
+          },
+        ],
+      })
+    );
+
+    return config;
+  },
+});
---
+++ utils/test/predict.ts
@@ -0,0 +1,11 @@
+import { getImageTensorFromPath } from './imageHelper';
+import { runSqueezenetModel } from './modelHelper';
+
+export async function inferenceSqueezenet(path: string): Promise<[any, number]> {
+  // 1. Convert image to tensor
+  const imageTensor = await getImageTensorFromPath(path);
+  // 2. Run model
+  const [predictions, inferenceTime] = await runSqueezenetModel(imageTensor);
+  // 3. Return predictions and the amount of time it took to inference.
+  return [predictions, inferenceTime];
+}
