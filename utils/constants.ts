c2f041b6decb67f1cb7e4f3de9dae8450c29a4b4
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-04-19 09:17:07 -0700

    store model in releases

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,8 @@ const GITHUB_PAGE_URL = "https://github.com/UC-Berkeley-I-School/mids-210-medfus
 const RELEASE_VERSION = "v0.0.1-alpha/";
 
 // export const NLP_MODEL_URL = "https://mids-210-medical-multi-modal-models.s3.us-west-2.amazonaws.com/bioclinicalbert_nlp_cls_best.onnx";
-export const NLP_MODEL_URL = "https://mids-210-medical-multi-modal-models.s3.us-west-2.amazonaws.com/bioclinicalbert_nlp_cls_best_fp16.onnx";
+// export const NLP_MODEL_URL = "https://mids-210-medical-multi-modal-models.s3.us-west-2.amazonaws.com/bioclinicalbert_nlp_cls_best_fp16.onnx";
+export const NLP_MODEL_URL = "./bioclinicalbert_nlp_cls_best_fp16.onnx";
 export const IMAGE_MODEL_URL = "./EfficientNet-B3_Model.onnx";
 export const TABULAR_MODEL_URL = "./pipeline_tabular_model.onnx";
 export const FUSION_MODEL_URL = './fusion_model_1a.onnx';
